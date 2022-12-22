832df3e8a76ac1d98f5207ca5f6242577f701ce0
sumeet2605 <75994952+sumeet2605@users.noreply.github.com>
2022-12-22 15:58:33 +0530

    dockerfile

--- index.js
+++ index.js
@@ -29,44 +29,50 @@ app.get("/webhook",(req,res)=>{
     }
 });
 
-app.post("/webhook",(req,res)=>{
+app.post("/webhook",(req,res)=>{ //i want some 
+
     let body_param=req.body;
 
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
+        console.log("inside body param");
         if(body_param.entry && 
             body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.message && 
-            body_param.entry[0].changes[0].value.message[0]){
-                let phone_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-                let from=body_param.entry[0].changes[0].value.message[0].from;
-                let msg_body=body_param.entry[0].changes[0].value.message[0].text.body;
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-                axios({
-                    method:"POST",
-                    url:"https://graph.facebook.com/v15.0/" +phone_no_id+"/message?access_token="+token,
-                    data:{
-                        messaging_product: "whatsapp",
-                        to:from,
-                        text:{
-                            body:"Hi.. Welcome to Alluring Lens by AR"
-                        }
-                    },
-                    headers:{
-                        "Content-Type":"application/json"
-                    },
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
 
-                    
-                });
-                res.sendStatus(200);
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
 
+               });
 
-
+               res.sendStatus(200);
             }else{
                 res.sendStatus(404);
             }
+
     }
+
 });
 
 app.get("/",(req,res)=>{
