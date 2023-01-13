2c56354751d9996f1e70cb4fa1a7fb318b7d40d0
sumeet2605 <75994952+sumeet2605@users.noreply.github.com>
2023-01-13 09:31:42 +0530

    test v5

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ async function detectIntentText(query) {
     console.log(
       `Text: ${text.trim()}`
     );
-  return response.queryResult.responseMessages;
+  return response;
   }
 
 app.post("/webhook",(req,res)=>{ //i want some 
@@ -104,7 +104,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                console.log("from "+from);
                console.log("boady param "+msg_body);
                
-               let responses = detectIntentText(msg_body);     
+               let responses =detectIntentText(msg_body);     
                
                console.log("Response: "+responses);
                
